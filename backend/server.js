fc952a0903a0be92926f0648562517b51382a77d
Gareth-Moore <gccsmoore3@gmail.com>
2023-09-20 14:27:57 +0100

    changed proxy

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://back-for-seconds-backend.onrender.com/",
+    origin: "https://back-for-seconds.onrender.com",
     credentials: true,
     methods: "GET, POST, PUT, DELETE",
   })
