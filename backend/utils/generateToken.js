c2d2ef238284dbffa5474d733df898f41d231ecd
Gareth-Moore <gccsmoore3@gmail.com>
2023-09-21 10:58:50 +0100

    changed proxy

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,16 +1,19 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
+  console.log(userId, "start token");
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
+    // secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
+  console.log(userId, "end token");
 };
 
 export default generateToken;
